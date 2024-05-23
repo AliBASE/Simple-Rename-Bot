38c3af2e78ae0f452ad59b3af47f33e17bf422d6
AliBASE <baselali90i@gmail.com>
2024-05-23 19:15:54 +0300

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ API_ID = int(environ.get("API_ID", "22256210"))
 API_HASH = environ.get("API_HASH", "f449225a769a47e9060f9c7c96314a4c")
 BOT_TOKEN = environ.get("BOT_TOKEN", "6561710912:AAG4ukgRoEhXPNdg4qgIqsjfKOON8-T-XWs")
 ADMIN = int(environ.get("ADMIN", "1002126514364"))          
-CAPTION = environ.get("CAPTION", "softbaylog")
+CAPTION = environ.get("CAPTION", "@softbaylog")
 
 # for thumbnail ( back end is MrMKN brain 😉)
 DOWNLOAD_LOCATION = "./DOWNLOADS"
